a2cec5a9b6b869e503db0ca012c6002ab51b3de9
Heloisa S. Rios <131817283+heloisasatira@users.noreply.github.com>
2024-06-09 14:26:01 -0300

    Update Header.js

--- src/app/Header.js
+++ src/app/Header.js
@@ -3,11 +3,11 @@ import Image from "next/image";
 
 export default function Header(){
     <header className={styles.cabecalho}> 
-    <h1>cabeçalho</h1>
     </header>
     return(
     <Image
       width={100} 
       height={100}
-      src={"https://upload.wikimedia.org/wikipedia/commons/c/c0/Schiaparelli_logo.png"}/>
+      position= 'right'
+      src={"https://upload.wikimedia.org/wikipedia/commons/c/c0/Schiaparelli_logo.png"} className="img"/>
     )};
